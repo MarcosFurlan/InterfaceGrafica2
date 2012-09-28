ee5d2084c0ed86f1032c2364aeaa3d9a4076e58d
Luiz Marcos <luizmarcosfurlanjunior82@hotmail.com>
2012-09-27 22:31:43 -0300

    fim aula dia 27

--- FilmesAssistidos/Form1.Designer.cs
+++ FilmesAssistidos/Form1.Designer.cs
@@ -170,7 +170,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(92, 25);
             this.button3.TabIndex = 11;
-            this.button3.Text = "Alterar";
+            this.button3.Text = "Gravar";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
--- FilmesAssistidos/Form1.cs
+++ FilmesAssistidos/Form1.cs
@@ -14,24 +14,31 @@ namespace FilmesAssistidos
     {
         Dictionary<string, List<FilmeA>> dic = new Dictionary<string, List<FilmeA>>();
         ListViewItem Item = new ListViewItem();
+        string EditarFilme;
+        int EditarGenero;
+        DateTime EditarData;
+        string EditarLocal;
+        
         
         public Form1()
         {
             InitializeComponent();
             
         }
-
+        
+        
         private void button1_Click(object sender, EventArgs e)
         {
             
-            string data = dateTimePicker1.Value.ToShortDateString();
 
-            FilmeA pegar = new FilmeA(txtFilme.Text, comboBox1.Text, data, txtLocal.Text);
-            
+           // DateTime DT = DateTime.ParseExact(dateTimePicker1.Text, "dd / MM / aaaa", null);
+           // DateTime data = dateTimePicker1.Value;
+            FilmeA pegar = new FilmeA(txtFilme.Text, comboBox1.Text ,  dateTimePicker1.Text , txtLocal.Text);
 
+             
             ListViewItem Item = new ListViewItem(txtFilme.Text);
             Item.SubItems.Add(comboBox1.Text);
-            Item.SubItems.Add(data);
+            Item.SubItems.Add(dateTimePicker1.Value.ToShortDateString());
             Item.SubItems.Add(txtLocal.Text);
             Item.Group = listView1.Groups[comboBox1.SelectedIndex -1];
 
@@ -83,28 +90,37 @@ namespace FilmesAssistidos
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (listView1.SelectedItems.Count > 0)
+
+
+            foreach (List<FilmeA> ValoresDicionario in dic.Values)
             {
-                txtFilme.Text = listView1.SelectedItems[0].Text;
-                listView1.SelectedItems[0].BeginEdit();
-                listView1.SelectedItems[0].Remove();
 
-
-
+                foreach (FilmeA PercorreObjeto in ValoresDicionario)
+                {
+                    if (PercorreObjeto.Nome == EditarFilme && PercorreObjeto.Genero == EditarGenero && PercorreObjeto.Local == EditarLocal && PercorreObjeto.Data == EditarData.ToShortDateString())
+                    {
+                        PercorreObjeto.Nome = txtFilme.Text;
+                        PercorreObjeto.Genero = comboBox1.Text;
+                        PercorreObjeto.Data = dateTimePicker1.Text;
+                        PercorreObjeto.Local = txtLocal.Text;
+                    }
+                }
             }
         }
 
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
 
-            if (listView1.SelectedItems.Count > 0)
+            foreach (ListViewItem SelecionaFilme in listView1.SelectedItems)
             {
-                txtFilme.Text = listView1.SelectedItems[0].Text;
-                listView1.SelectedItems[0].BeginEdit();
-                listView1.SelectedItems[0].Remove();
-                
-
-
+                txtFilme.Text = SelecionaFilme.Text;
+                comboBox1.Text = SelecionaFilme.Group.Header;
+               // dateTimePicker1 = SelecionaFilme.SubItems[1].Text;
+                txtLocal.Text = SelecionaFilme.SubItems[2].Text;
+                EditarFilme = SelecionaFilme.Text;
+               // EditarData = Convert.ToDateTime(SelecionaFilme.SubItems[1].Text);
+                EditarGenero = listView1.Groups.IndexOf(SelecionaFilme.Group);
+                EditarLocal = SelecionaFilme.SubItems[2].Text;
             }
 
             
